1f84340589d8f53be37911b38b9fde73ab702029
raeoffrost <raeoffrost@gmail.com>
2025-02-21 21:11:39 -0600

    Add form to main and add some styles

--- pages/form.js
+++ pages/form.js
@@ -2,33 +2,64 @@ import React, { useState } from "react";
 import Email from "@/component/email";
 import Name from "@/component/name";
 import Password from "@/component/password";
+import styles from "@/styles/Form.module.css";
 
 const Form = () => {
-  const [values, setValues] = useState("");
+  const [message, setMessage] = useState("");
+  const initialValues = { name: "", email: "", password: "" };
+  const [formValues, setFormValues] = useState(initialValues);
+  const [formErrors, setFormErrors] = useState("");
+
   const handleSubmit = (e) => {
     // prevent default submit
     e.preventDefault();
-    if (!values) {
-      setMessage("ERROR: Please fill form.");
-    } else setMessage("Form submitted successfully!");
+    if (formValues == initialValues) {
+      setFormErrors("ERROR: Please fill all form inputs.");
+    } else {
+      setMessage("Form submitted successfully!");
+      setFormErrors("");
+    }
   };
+
   const formStyle = {
     padding: "20px",
     font: "inherit",
   };
+  const btnStyle = {
+    borderRadius: 5,
+    border: "1px solid grey",
+    padding: 10,
+    marginTop: 5,
+    marginLeft: 10,
+    fontFamily: "inherit",
+  };
+  const errorStyle = {
+    color: "#ff8b8b",
+    marginTop: 10,
+    marginLeft: 10,
+  };
+  const messageStyle = {
+    color: "green",
+    marginTop: 10,
+    marginLeft: 10,
+  };
   return (
     <>
       <form onSubmit={handleSubmit} style={formStyle}>
-        <Name />
-        <Email />
-        <Password />
+        <Name onChange={(e) => setFormValues(e.target.value.Name)} className={styles.form} />
         <br />
-        <button
-          type="submit"
-          style={{ padding: 10, marginTop: 10, fontFamily: "inherit", borderRadius: 5 }}
-        >
+        <Email onChange={(e) => setFormValues(e.target.value.Email)} className={styles.form} />
+        <br />
+        <Password
+          onChange={(e) => setFormValues(e.target.value.Password)}
+          className={styles.form}
+        />
+        <br />
+        <button type="submit" style={btnStyle}>
           Submit
         </button>
+        <div style={errorStyle}>{formErrors && <p>{formErrors}</p>}</div>
+        <div style={messageStyle}>{message && <p>{message}</p>}</div>
       </form>
     </>
   );
