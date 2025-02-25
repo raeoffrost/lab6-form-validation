dbba06ff6613b73e417fe4be4180b9fd96acd3ba
raeoffrost <raeoffrost@gmail.com>
2025-02-24 21:06:57 -0600

    Fized validation to display under correct inputs

--- pages/form.js
+++ pages/form.js
@@ -8,28 +8,45 @@ const Form = () => {
   const [message, setMessage] = useState("");
   const initialValues = { name: "", email: "", password: "" };
   const [formValues, setFormValues] = useState(initialValues);
-  const [formErrors, setFormErrors] = useState("");
+  const [formErrors, setFormErrors] = useState([
+    { id: 0, message: "", active: false },
+    { id: 1, message: "ERROR: Please provide a name.", active: false },
+    { id: 2, message: "ERROR: Please provide an email.", active: false },
+    { id: 3, message: "ERROR: Provide a valid email.", active: false },
+    { id: 4, message: "ERROR: Password must be more than 6 characters.", active: false },
+  ]);
 
   const handleSubmit = (e) => {
     // prevent default submit
     e.preventDefault();
-    if (!formValues.email) {
-      setFormErrors("ERROR: Please provide an email.");
+    if (!formValues.name) {
+      updateError(1);
       setMessage("");
-    } else if (!formValues.name) {
-      setFormErrors("ERROR: Please provide a name.");
-      setMessage("");
-    } else if (formValues.password.length < 6) {
-      setFormErrors("ERROR: password must be more that 6 characters.");
+    } else if (!formValues.email) {
+      updateError(2);
       setMessage("");
     } else if (!formValues.email.includes("@") || !formValues.email.includes(".")) {
-      setFormErrors("ERROR: provide a valid email");
+      updateError(3);
+      setMessage("");
+    } else if (formValues.password.length < 6) {
+      updateError(4);
       setMessage("");
     } else {
       setMessage("Form submitted successfully!");
-      setFormErrors("");
+      updateError(0);
     }
   };
+
+  function updateError(id) {
+    const updatedErrors = formErrors.map((error) => {
+      if (error.id === id) {
+        return { ...error, active: true };
+      } else {
+        return { ...error, active: false };
+      }
+    });
+    setFormErrors(updatedErrors);
+  }
 
   const handleChange = (e) => {
     const name = e.target.name;
@@ -60,17 +77,37 @@ const Form = () => {
   };
   return (
     <>
-      <form onSubmit={handleSubmit} style={formStyle}>
+      <form onSubmit={handleSubmit} style={formStyle} noValidate>
         <Name value={formValues.name} onChange={handleChange} className={styles.form} />
+        {formErrors.find((error) => error.id === 1 && error.active) && (
+          <div style={errorStyle}>
+            <p>{formErrors.find((error) => error.id === 1).message}</p>
+          </div>
+        )}
         <br />
+
         <Email value={formValues.email} onChange={handleChange} className={styles.form} />
+        {formErrors.find((error) => error.id === 2 && error.active) && (
+          <div style={errorStyle}>
+            <p>{formErrors.find((error) => error.id === 2).message}</p>
+          </div>
+        )}
+        {formErrors.find((error) => error.id === 3 && error.active) && (
+          <div style={errorStyle}>
+            <p>{formErrors.find((error) => error.id === 3).message}</p>
+          </div>
+        )}
         <br />
-        <Password onChange={handleChange} className={styles.form} />
+        <Password value={formValues.password} onChange={handleChange} className={styles.form} />
+        {formErrors.find((error) => error.id === 4 && error.active) && (
+          <div style={errorStyle}>
+            <p>{formErrors.find((error) => error.id === 4).message}</p>
+          </div>
+        )}
         <br />
         <button type="submit" style={btnStyle}>
           Submit
         </button>
-        <div style={errorStyle}>{formErrors && <p>{formErrors}</p>}</div>
         <div style={messageStyle}>{message && <p>{message}</p>}</div>
       </form>
     </>
